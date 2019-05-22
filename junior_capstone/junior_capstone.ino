ed74b20b358b87735e99242f8e4b307660525c19
HanKyeol Kyung <kghnkl0103@gmail.com>
2019-05-22 15:39:51 +0900

    배열로 코드 간략화, 파싱부분 고침

--- junior_capstone/junior_capstone.ino
+++ junior_capstone/junior_capstone.ino
@@ -1,13 +1,9 @@
 #include <Stepper.h>
 #include <math.h>
-const int steps_per_rev = 200; //1스텝당 1.8도 회전 step/rev
+const int steps_per_rev = 200; //모터 자체는 1스텝당 1.8도 회전 step/rev
 const int delay_const = 200; //1스텝당 딜레이 시간
-const int xy_motor = 1000; //한바퀴 돌때 1000um 이동 um/rev
-const int rotate_motor = 30; //한바퀴 돌때 30도 회전
-const float angle_motor = 7.2; //한바퀴 돌때 7.2도 이동
-const float xy_step = 5; //1스텝에 5um 이동
-const float rotate_step = 0.15; //1스텝에 0.15도 회전
-const float tilting_step = 0.036; //1스텝에 0.036도 회전
+const float step_index[3] = {5, 0.15, 0.036}; //xy는 1스텝에 5미리, 회전은 1스텝에 0.15도, 틸팅은 1스템에 0.036도
+int i;
 
 int command_parsing(String command, int command_nums[]);
 
@@ -18,11 +14,12 @@ int command_parsing(String command, int command_nums[]);
 ////////////////////////////////////////////////////////
 
 // initialize the stepper
-Stepper motor1(steps_per_rev, 0, 1, 2, 3);//TODO 핀번호 알맞게
-Stepper motor2(steps_per_rev, 4, 5, 6, 7);//TODO 핀번호 알맞게
-Stepper motor3(steps_per_rev, 8, 9, 10, 11);//TODO 핀번호 알맞게
-Stepper motor4(steps_per_rev, 12, 13, 14, 15);//TODO 핀번호 알맞게
-Stepper motor5(steps_per_rev, 16, 17, 18, 19);//TODO 핀번호 알맞게
+Stepper stepper1(steps_per_rev, 0, 1, 2, 3);//TODO 핀번호 알맞게
+Stepper stepper2(steps_per_rev, 4, 5, 6, 7);//TODO 핀번호 알맞게
+Stepper stepper3(steps_per_rev, 8, 9, 10, 11);//TODO 핀번호 알맞게
+Stepper stepper4(steps_per_rev, 12, 13, 14, 15);//TODO 핀번호 알맞게
+Stepper stepper5(steps_per_rev, 16, 17, 18, 19);//TODO 핀번호 알맞게
+Stepper steppers[] = {stepper1, stepper2, stepper3, stepper4, stepper5};
 
 void setup() {
   // initialize the serial port:
@@ -30,42 +27,30 @@ void setup() {
 }
 
 void loop() {
-  int command_nums[2];
+  int command_nums[2];//숫자로 된 명령어
   int command_code = 0;
-  int motor_change[3] = 0
-                        String command;
+  int motor_change[3] = {0, 0, 0};
+  String command;
   Serial.println("Please enter command");//명령어를 입력해주세요
   if (Serial.available() > 0) //시리얼이 연결된다면
     command = Serial.readStringUntil('\n');
-
   command_code = command_parsing(command, command_nums);
+  
   if (command_code == 1 ) { //모터회전
-    switch (command_nums[0]) {
-      case 1: motor1.step(command_nums[1]);
-        motor_change[0] -= command_nums[1];
-        break;
-      case 2: motor2.step(command_nums[1]);
-        motor_change[1] -= command_nums[1];
-        break;
-      case 3: motor3.step(command_nums[1]);
-        motor_change[2] -= command_nums[1];
-        break;
-      case 4: motor4.step(command_nums[1]);
-        break;
-      case 5: motor3.step(command_nums[1]);
-        motor5.step(-command_nums[1]);
-        break;
+    steppers[command_nums[0]].step(command_nums[1]);
+    if (command_nums[0] < 3)
+      motor_change[command_nums[0]] -= command_nums[1];
+  }
+  
+  else if (command_code == 2) {//원래대로 움직이게 하는 부분
+    for (i = 0; i < 3; i++) {
+      steppers[i].step(motor_change[i]);
+      motor_change[i] = 0;
+      delay(2000);
     }
   }
-  else if (command_code == 2) {
-    motor1.step(motor_change[0]);
-    motor2.step(motor_change[1]);
-    motor3.step(motor_change[2]);
-    motor_change[0] = 0;
-    motor_change[1] = 0;
-    motor_change[2] = 0;
-  }
-  else {
+  
+  else {//파싱하다가 문제있을때
     Serial.println("ERROR while command parsing");
   }
   //    delay(command_nums[1] * 100); //step 마다 0.1초 대기
@@ -84,8 +69,8 @@ int command_parsing(String command, int command_nums[]) {
   int index2 = command.indexOf('/', index1 + 1);
   int index3 = command.length();
 
-  char command1 = command.substring(index2 + 1, index3).charAt(0);
-  char command2 = command.substring(index2 + 1, index3).charAt(0);
+  char command1 = command.substring(0, index1).charAt(0);
+  char command2 = command.substring(index1 + 1, index2).charAt(0);
   float command3 = command.substring(index2 + 1, index3).toFloat();
 
   command1 = (char) tolower(command1);
@@ -93,61 +78,61 @@ int command_parsing(String command, int command_nums[]) {
 
   switch (command1) {
     case 'x': //x방향 이동
-      if (fmodf(command3, xy_step) == 0.0) {
-        command_nums[0] = 1;
-        command_nums[1] = (int)(negative_flag * command3 / xy_step);
+      if (fmodf(command3, step_index[0]) == 0.0) {
+        command_nums[0] = 0;
+        command_nums[1] = (int)(negative_flag * command3 / step_index[0]);
         Serial.print("Moving to X-Direction ");
-        Serial.print(negative_flag * command3 / xy_step);
+        Serial.print(negative_flag * command3 / step_index[0]);
         Serial.println(" step(s)");
       }
       else {
         Serial.print("ERROR:Number can`t be divided into ");
-        Serial.println(xy_step);
+        Serial.println(step_index[0]);
         return 0;
       }
       break;
 
     case 'y': //y방향 이동
-      if (fmodf(command3, xy_step) == 0.0) {
-        command_nums[0] = 2;
-        command_nums[1] = (int)(negative_flag * command3 / xy_step);
+      if (fmodf(command3, step_index[0]) == 0.0) {
+        command_nums[0] = 1;
+        command_nums[1] = (int)(negative_flag * command3 / step_index[0]);
         Serial.print("Moving to Y-Direction ");
-        Serial.print(negative_flag * command3 / xy_step);
+        Serial.print(negative_flag * command3 / step_index[0]);
         Serial.println(" step(s)");
       }
       else {
         Serial.print("ERROR:Number can`t be divided into ");
-        Serial.println(xy_step);
+        Serial.println(step_index[0]);
         return 0;
       }
       break;
 
     case 'r': //회전
-      if (fmodf(command3, rotate_step) == 0.0) {
-        command_nums[0] = 3;
-        command_nums[1] = (int)(negative_flag * command3 / rotate_step);
+      if (fmodf(command3, step_index[1]) == 0.0) {
+        command_nums[0] = 2;
+        command_nums[1] = (int)(negative_flag * command3 / step_index[1]);
         Serial.print("Rotating ");
-        Serial.print(negative_flag * command3 / rotate_step);
+        Serial.print(negative_flag * command3 / step_index[1]);
         Serial.println(" step(s)");
       }
       else {
         Serial.print("ERROR:Number can`t be divided into ");
-        Serial.println(rotate_step);
+        Serial.println(step_index[1]);
         return 0;
       }
       break;
 
     case 't': //기울이기
-      if (fmodf(command3, tilting_step) == 0.0) {
-        command_nums[0] = 4;
-        command_nums[1] = (int)(negative_flag * command3 / tilting_step);
+      if (fmodf(command3, step_index[2]) == 0.0) {
+        command_nums[0] = 3;
+        command_nums[1] = (int)(negative_flag * command3 / step_index[2]);
         Serial.print("Tilting ");
-        Serial.print(negative_flag * command3 / tilting_step);
+        Serial.print(negative_flag * command3 / step_index[2]);
         Serial.println(" step(s)");
       }
       else {
         Serial.print("ERROR:Number can`t be divided into ");
-        Serial.println(tilting_step);
+        Serial.println(step_index[2]);
         return 0;
       }
       break;
